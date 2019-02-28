1d8532ef1ee567bd24e9540aa394e265f527763e
sanjeev <sanjeevsiva17@gmail.com>
2019-02-28 15:52:30 +0530

    Modularised emailgen code

--- test/emailgen.py
+++ test/emailgen.py
@@ -7,51 +7,40 @@ from email import Encoders
 import datetime
 import re 
 
-html_data = """
-        <html>
-            <head></head>
-            <body>
-                <p> hello world </p>
-            </body>
-        </html>
-        """
+def Emailgen(From, to, subject, cc, bcc, *attachments):
+    html_data = """
+            <html>
+                <head></head>
+                <body>
+                    <p> hello world </p>
+                </body>
+            </html>
+            """
 
-msg = MIMEMultipart('alternative')
-msg['Subject'] = raw_input('Subject: ')
+    msg = MIMEMultipart('alternative')
+    msg['Subject'] = subject
 
-From = raw_input('From: ')
-msg['From'] = '"hello world!," <'+ From +'>'
-# +re.findall(r"^[^@]+", From)+
-msg['To'] = raw_input('To: ')
-msg['Cc'] = raw_input('Cc: ')
-msg['Bcc'] = raw_input('Bcc: ')
-msg['Date'] = str(datetime.datetime.now())
-part = MIMEText(html_data, 'html')
-msg.attach(part)
-
-
-
-part = MIMEBase('application', "octet-stream")
-part.set_payload(open("test.json", "rb").read())
-Encoders.encode_base64(part)
-
-part.add_header('Content-Disposition', 'attachment; filename="test.json"')
-
-msg.attach(part)
-
-# headers = ... dict of header key / value pairs ...
+    From = From
+    msg['From'] = '"hello world!," <'+ From +'>'
+    # +re.findall(r"^[^@]+", From)+
+    msg['To'] = to
+    msg['Cc'] = cc
+    msg['Bcc'] = bcc
+    msg['Date'] = str(datetime.datetime.now())
+    part = MIMEText(html_data, 'html')
+    msg.attach(part)
 
 
-# for key in headers:
-#     value = headers[key]
-#     if value and not isinstance(value, basestring):
-#         value = str(value)
-#     msg[key] = value
+    for file in attachments:
+        part = MIMEBase('application', "octet-stream")
+        part.set_payload(open(file, "rb").read())
+        Encoders.encode_base64(part)
+        part.add_header('Content-Disposition', 'attachment; filename=' + file)
+        msg.attach(part)
 
+    outfile_name = ("eml/email_sample.eml")
+    with open(outfile_name, 'w') as outfile:
+        gen = generator.Generator(outfile)
+        gen.flatten(msg)
 
-outfile_name = ("eml/email_sample.eml")
-with open(outfile_name, 'w') as outfile:
-    gen = generator.Generator(outfile)
-    gen.flatten(msg)
-
-print ("=========== DONE ============")
+    print ("=========== DONE ============")
