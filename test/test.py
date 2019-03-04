b38df7bb2f9e91701815f5ad80ef87cb4f357ad8
sanjeev <sanjeevsiva17@gmail.com>
2019-03-04 12:27:48 +0530

    Added test code

--- test/test.py
+++ test/test.py
@@ -3,11 +3,12 @@ import botocore
 import json
 import os
 from emailgen import Emailgen
+import collections
 
 s3 = boto3.resource('s3', region_name = 'us-east-1')
 
-bucket_name = 'email_blob_bucket'
-bucket_name1 = 'email_attachment_bucket'
+bucket_name = 'inbox-bucket-test'
+bucket_name1 = 'inbox-bucket-testresized'
 
 bucket = s3.Bucket(bucket_name)
 print("created source bucket\n")
@@ -21,21 +22,29 @@ def upload(filename):
   key = filename
   bucket.upload_file(fileToBeUploaded, key)
 
-def assertAttachment(fromaddr, *files):
-  for object in bucket1.objects.all():
-    x = []
-    x.append(object)
-    print(object)
+compare = lambda x, y: collections.Counter(x) == collections.Counter(y)    
+
+def assertAttachment(From, *attachments):
+    objectlist = []
+    for key in bucket1.objects.filter(Prefix=From): 
+      objectlist = (objectlist + key.key.split("/")[-1:])
+    print(objectlist)   
+    result = compare(attachments,objectlist)   
+    if(result == True):
+      print("Succesfully extracted ataachments")
+      print(objectlist)  
+    else:
+      print("Unsucessfull, Check cloudview logs for more details!")
 
 def test1(From, to, Sub, cc, bcc, *attchments):
   emailBlob = Emailgen(From, to, Sub, cc, bcc, *attchments)
   
   print("Updating bucket information")
   print("Uploading to bucket\n")
-  upload("email_sample.eml")
+  upload(From)
 
   print("Checking Destination bucket\n")
-  assertAttachment('sanjeevsiva17', 'emailgen.py', 'lambda.py')
+  assertAttachment(From, *attachments)
 
 From = raw_input("From : ")
 To = raw_input("To : ")
@@ -49,3 +58,4 @@ for i in range(number_of_attachments):
   attachments.append(raw_input("Enter name of attachment: "))
 
 test1(From, To, Sub, cc, bcc, *attachments)
+# assertAttachment('sanjeevsiva17@gmail.com', 'test.json')
